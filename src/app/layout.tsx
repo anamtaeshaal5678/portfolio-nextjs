1ba6764454f870be30cdaa407f4c3d23554d2ae5
anamtaeshaal5678 <mahwishkhan56789@gmail.com>
2024-11-09 23:30:13 +0500

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Footer from "./component/Footer";
-import Contact from "./component/Contact";
-import About from "./component/About";
-import Hero from "./component/Hero";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
